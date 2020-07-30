4248ac178921c63fc687c31db4c96e7370f0028e
Thanh Trung <thanhgnurt@gmail.com>
2020-07-30 20:55:51 +0700

    commit first

---
+++ Console_C_Sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y,z;
+            float mean;
+            Console.WriteLine("nhap x:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri y :");
+            y = int.Parse(Console.ReadLine());
+            z = x + y;
+            Console.WriteLine("Tong hai so {0},{1} la: {2} ", x, y, z) ;
+            mean = (float)(x + y) / 2;
+            Console.WriteLine("Trung binh cong la : {0}", mean);
+
+
+
+        }
+    }
+}
---
+++ Swaps_Value/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Swaps_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // trao doi gia tri
+            int a, b, temp;
+            Console.WriteLine("Nhap gia tri a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri b :");
+            b = int.Parse(Console.ReadLine());
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("Gia tri sau khi duoc thay doi: a = {0} va b = {1}", a, b);
+        }
+    }
+}
