4f87f62eea5625d76ae1ea8b0ed355ba0575536b
Thanh Trung <thanhgnurt@gmail.com>
2020-07-30 23:04:47 +0700

    mutiplication table another

---
+++ mutiplication_table_another/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mutiplication_Table_Another
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Program p = new Program();
+            Console.WriteLine("MUTIPLICATION TABLE");
+            for (; ; )
+            {
+                try
+                {
+                    Console.WriteLine("Please!! Enter number (note : number is a integer not 0");
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch { Console.WriteLine("Enter error . Please again."); }
+            }
+            
+            p.mutiplication_table(number);
+
+        }
+
+        void mutiplication_table(int number)
+        {
+            for (int i = 0; i <=10; i++)
+            {
+
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+        }
+
+
+
+
+    }
+}
