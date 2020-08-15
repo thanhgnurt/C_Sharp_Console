dc9b76092be20f39e94bdef37280438695fcc195
Thanh Trung <thanhgnurt@gmail.com>
2020-08-15 21:42:25 +0700

    commit ngay 15/8

---
+++ SumOneToN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace SumOneToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hay nhap vao so n");
+            int n = int.Parse(Console.ReadLine());
+            int result = 1;
+            
+            while (n/10>0)
+            {
+                result++;
+                n = n / 10;
+
+            }
+            
+
+            
+
+            Console.WriteLine("ket qua la:{0}", result);
+        }
+    }
+}
